eb63ef8295a9030ec6c1764c6aeca04d98b02d15
dy258 <dy258@cornell.edu>
2020-02-18 10:50:52 -0500

    add warning message

--- seizureDetection/ViewController.swift
+++ seizureDetection/ViewController.swift
@@ -11,10 +11,24 @@ import UIKit
 class ViewController: UIViewController {
     
     
+    @IBOutlet weak var termCheckBox: Checkbox!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
+
+    @IBAction func continueToMain(_ sender: UIButton) {
+        if !termCheckBox.isChecked {
+            let termAlert = UIAlertController(title: "Please agree to the terms and conditions before proceeding", message: "", preferredStyle: UIAlertController.Style.alert)
+            print("here 1")
+            termAlert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { (action) in termAlert.dismiss(animated: true, completion: nil)}))
+            print("here 2")
+            self.present(termAlert, animated: true, completion: nil)
+            
+            print("here 3")
+        }
+    }
 }
 
